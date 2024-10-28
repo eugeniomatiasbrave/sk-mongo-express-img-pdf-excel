c2409b6453c26046dfe36f62d980d4eeed60e036
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2024-10-28 14:47:06 -0300

    app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import path from "path";
 import  __dirname from "./utils.js";
 import productsRouter from "./routes/products.router.js";
 import writingsRouter from "./routes/writings.router.js";
 import mongoose from 'mongoose';
+import cors from 'cors';
 import config from './config/config.js';
 
 const app = express();
@@ -11,11 +13,9 @@ const PORT = config.app.PORT;
 
 mongoose.connect(config.mongo.URL);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public'))); // Servir archivos estáticos desde la carpeta 'public'
@@ -23,3 +23,8 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Servir 
 
 app.use('/api/products', productsRouter);
 app.use('/api/writings', writingsRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
