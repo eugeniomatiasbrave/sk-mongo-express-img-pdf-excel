1f6d6d2123f8ce8fb3e9ad4662c2df02308f4fe3
eugeniomatiasbrave <107762123+eugeniomatiasbrave@users.noreply.github.com>
2024-10-28 15:31:52 -0300

    Update npmignore files for various dependencies

--- server/src/managers/mongo/WritingsManagers.js
+++ server/src/managers/mongo/WritingsManagers.js
@@ -3,16 +3,19 @@ import writingModel from './models/writings.model.js';
 
 export default class WritingsManagers {
 	
-    getWritings() {
+  getWritings() {
 		return writingModel.find({}).lean();
 	};
 
-    createWriting(wtn){ // Crea uno nuevo
-        return writingModel.create(wtn);
-    };
+  createWriting(wtn){ // Crea uno nuevo
+    return writingModel.create(wtn);
+  };
 
-    getWritingById (wid) { // Busca solo uno
+  getWritingById (wid) { // Busca solo uno
 		return writingModel.findOne({_id:wid}).lean(); 
 	};
 
+  deleteWriting (wid) { // Elimina uno
+    return writingModel.deleteOne({_id:wid});
+  }
 };
